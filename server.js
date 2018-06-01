55d2c565a3a084e35468529f26ce2fa614b6e367
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-02 05:03:03 +0700

    setup for file structure completed and imported sub-apps and set server routing paths

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const cors = require('cors');
 const port = 5555;
 const server = express();
 server.use(express.json());
-// server.use(cors({ origin: "http://localhost:3001" }));
+// server.use(cors({ origin: "http://localhost:3003" }));
 
 // middleware
 
 // import sub-applications
+const actionModel = require('./actions/actionModel');
+const projectModel = require('./projects/projectModel');
 
 // route handlers
+server.use('/actions', actionModel);
+server.use('/projects', projectModel);
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
